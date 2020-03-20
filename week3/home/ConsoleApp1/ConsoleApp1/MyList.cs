fc7c94ba7279375b153f5230df6b01840cc525a3
daniel.burghelea <daniel.burghelea@webbeds.com>
2020-03-20 14:48:37 +0200

    first try ex 1 week 3

---
+++ week3/home/ConsoleApp1/ConsoleApp1/MyList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+	class MyList<T>
+	{
+		private int index;
+		private int count;
+		private T[] data;
+
+		public MyList()
+		{
+			Initialize();
+		}
+
+		private void Initialize()
+		{
+			count = 0;
+			index = 2;
+			data = new T[index];
+		}
+
+		public void Add(T item)
+		{
+			CheckAndAllocateArrayMemory();
+
+			if (data.Length > count && data[count] == null)
+			{
+				data[count] = item;
+				count++;
+			}
+		}
+
+		public void Clear()
+		{
+			Initialize();
+		}
+
+		public void RemoveAt(int i)
+		{
+			for (int a = i; a < data.Length - 1; a++)
+			{
+				data[a] = data[a + 1];
+			}
+
+			count--;
+		}
+
+		public T[] GetList()
+		{
+			return data;
+		}
+
+		private void CheckAndAllocateArrayMemory()
+		{
+			if (count >= data.Length)
+			{
+				index = index * 2;
+				Array.Resize(ref data, index);
+			}
+		}
+	}
+}
---
+++ week3/home/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyList<string> list = new MyList<string>();
+			list.Add("aaa");
+			list.Add("bbb");
+			list.Add("ccc");
+			list.Add("ddd");
+			list.Add("eee");
+
+			list.RemoveAt(2);
+		}
+	}
+}
