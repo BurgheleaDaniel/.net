f845e264aa8cbe73e21e921418851131b5a37e22
daniel.burghelea <daniel.burghelea@webbeds.com>
2020-03-20 16:09:21 +0200

    ex2 first try

---
+++ week3/home/ConsoleApp1/Timer/MyTimer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace Timer
+{
+	class MyTimer
+	{
+		public delegate void MessageDelegate(string str);
+
+		private MessageDelegate msgDel = Notify;
+
+		private string message;
+
+		private int seconds;
+
+		public MyTimer(string msg, int sec)
+		{
+			message = msg;
+			seconds = sec;
+		}
+
+		public void Run()
+		{
+			while (true)
+			{
+				msgDel(message);
+				Thread.Sleep(seconds * 1000);
+			}
+		}
+		 
+		static void Notify(string str)
+		{
+			Console.WriteLine($"Notification received for: {str}");
+		}
+	}
+}
