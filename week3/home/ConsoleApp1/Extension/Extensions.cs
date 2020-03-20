ebc09df75e43f1afdd8fe4af249103367959e165
daniel.burghelea <daniel.burghelea@webbeds.com>
2020-03-20 17:18:38 +0200

    ex 3 first try

---
+++ week3/home/ConsoleApp1/Extension/Extensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extension
+{
+	public static class Extensions
+	{
+		public static T Sum<T>(this IEnumerable<T> list)
+		{
+			T suma = (dynamic)0;
+			foreach (var item in list)
+			{
+				suma += (dynamic)item;
+			}
+			return suma;
+		}
+
+		public static T Product<T>(this IEnumerable<T> list)
+		{
+			T product = (dynamic)1;
+			foreach (var item in list)
+			{
+				product *= (dynamic)item;
+			}
+			return product;
+		}
+
+		public static T Min<T>(this IEnumerable<T> list)
+		{
+			T min = (dynamic)(new List<T>(list)[0]);
+			foreach (var item in list)
+			{
+				if (min > (dynamic)item)
+				{
+					min = (dynamic)item;
+				}
+			}
+			return min;
+		}
+
+		public static T Max<T>(this IEnumerable<T> list)
+		{
+			T max = (dynamic)(new List<T>(list)[0]);
+			foreach (var item in list)
+			{
+				if (max < (dynamic)item)
+				{
+					max = (dynamic)item;
+				}
+			}
+			return max;
+		}
+
+		public static T Average<T>(this IEnumerable<T> list)
+		{
+			return (dynamic)list.Sum() / new List<T>(list).Count;
+		}
+
+	}
+}
---
+++ week3/home/ConsoleApp1/Extension/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Extension
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> list = new List<int>();
+			list.Add(1);
+			list.Add(2);
+			list.Add(3);
+			list.Add(4);
+			list.Add(5);
+
+			Console.WriteLine(list.Sum());
+			Console.WriteLine(list.Product());
+			Console.WriteLine(list.Min());
+			Console.WriteLine(list.Max());
+			Console.WriteLine(list.Average());
+		}
+	}
+}
