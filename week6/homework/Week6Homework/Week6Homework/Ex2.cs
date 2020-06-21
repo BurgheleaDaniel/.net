323d9f62a241cd3779aed79a2ff9f9f91476f497
daniel.burghelea <daniel.burghelea@webbeds.com>
2020-06-21 23:34:04 +0300

    ex1 & ex2

---
+++ week6/homework/Week6Homework/Week6Homework/Ex2.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Week6Homework
+{
+	class Ex2
+	{
+		static async Task Main(string[] args)
+		{
+			ReadPostsAndComments commentsReader = new ReadPostsAndComments();
+			await commentsReader.RunAsync();
+			commentsReader.PrintAll();
+			Console.ReadKey();
+		}
+	}
+
+	class ReadPostsAndComments
+	{
+		private List<Dictionary<int, string>> CommentList = new List<Dictionary<int, string>>();
+
+		public async Task RunAsync()
+		{
+			List<int> idList = new List<int>();
+			var t1 = ReadAllPosts();
+
+			string[] posts = await Task.WhenAll(t1);
+			JArray details = JArray.Parse(posts[0]);
+
+			foreach (var element in details)
+			{
+				if (int.TryParse((string)element["id"], out int postId))
+				{
+					idList.Add(postId);
+				}
+			}
+
+			int proccessedPosts = 25;
+			var taskComments1 = ReadAllCommentsForAPost(idList, 0, proccessedPosts);
+			var taskComments2 = ReadAllCommentsForAPost(idList, 25, proccessedPosts);
+			var taskComments3 = ReadAllCommentsForAPost(idList, 50, proccessedPosts);
+			var taskComments4 = ReadAllCommentsForAPost(idList, 75, proccessedPosts);
+
+			await Task.WhenAll(taskComments1, taskComments2, taskComments3, taskComments4);
+		}
+
+		private async Task<string> ReadAllPosts()
+		{
+			HttpClient client = new HttpClient();
+			HttpResponseMessage response = await client.GetAsync($"https://jsonplaceholder.typicode.com/posts");
+			string value = await response.Content.ReadAsStringAsync();
+
+			return value;
+		}
+
+		private async Task ReadAllCommentsForAPost(List<int> idList, int start, int proccess)
+		{
+			Dictionary<int, string> dictionary = new Dictionary<int, string>();
+
+			for (int i = start; i < start + proccess; i++)
+			{
+				int id = idList[i];
+				HttpClient client = new HttpClient();
+				HttpResponseMessage response = await client.GetAsync($"https://jsonplaceholder.typicode.com/comments?postId={id}");
+				string value = await response.Content.ReadAsStringAsync();
+
+				dictionary.Add(id, value);
+			}
+
+			CommentList.Add(dictionary);
+		}
+
+		public void PrintAll()
+		{
+			foreach (Dictionary<int, string> dict in CommentList)
+			{
+				dict.ToList().ForEach(x => Console.WriteLine(x.Value));
+			}
+		}
+	}
+}
+
---
+++ week6/homework/Week6Homework/Week6Homework/UiForm.cs
@@ -0,0 +1,91 @@
+﻿namespace Week6Homework
+{
+	using System.Diagnostics;
+	using System.Net;
+	using System.Threading;
+	using System.Threading.Tasks;
+	using System.Windows.Forms;
+
+	public partial class UiForm : Form
+	{
+		public UiForm()
+		{
+			this.InitializeComponent();
+		}
+
+		private void DownloadBtnLeft_Click(object sender, System.EventArgs e)
+		{
+			var url = this.urlTextBoxLeft.Text;
+
+			var stopwatch = new Stopwatch();
+			stopwatch.Start();
+
+			var uiScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+
+			var t = new Task<string>(() =>
+			{
+				return this.DownloadString(url);
+			});
+
+			t.ContinueWith(prev =>
+			{
+				this.contentTxbLeft.Text = prev.Exception.Message;
+				this.logLabelLeft.Text = $@"Downloaded in {stopwatch.ElapsedMilliseconds} ms";
+			}, CancellationToken.None, TaskContinuationOptions.OnlyOnFaulted, uiScheduler);
+
+			t.ContinueWith(prev =>
+			{
+				this.contentTxbRight.Text = prev.Result;
+				this.logLabelRight.Text = $@"Downloaded in {stopwatch.ElapsedMilliseconds} ms";
+			}, CancellationToken.None, TaskContinuationOptions.NotOnFaulted, uiScheduler);
+
+			t.Start();
+
+		}
+
+		private async void DownloadBtnRight_Click(object sender, System.EventArgs e)
+		{
+			var url = this.urlTextBoxRight.Text;
+
+			var stopwatch = new Stopwatch();
+			stopwatch.Start();
+
+			Task<string> task = this.DownloadStringAsync(url);
+			var result = await task;
+
+			this.contentTxbRight.Text = result;
+
+			/*var uiScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+
+			var t = new Task<string>(() =>
+			{
+				return this.DownloadString(url);
+			});
+
+			t.ContinueWith(prev =>
+			{
+				this.contentTxbRight.Text = prev.Exception.Message;
+				this.logLabelRight.Text = $@"Downloaded in {stopwatch.ElapsedMilliseconds} ms";
+			}, CancellationToken.None, TaskContinuationOptions.OnlyOnFaulted, uiScheduler);
+
+			t.ContinueWith(prev =>
+			{
+				this.contentTxbRight.Text = prev.Result;
+				this.logLabelRight.Text = $@"Downloaded in {stopwatch.ElapsedMilliseconds} ms";
+			}, CancellationToken.None, TaskContinuationOptions.NotOnFaulted, uiScheduler);
+
+			t.Start();*/
+
+		}
+
+		private string DownloadString(string url)
+		{
+			return new WebClient().DownloadString(url);
+		}
+
+		private Task<string> DownloadStringAsync(string url)
+		{
+			return Task.Run(() => { return DownloadString(url); });
+		}
+	}
+}
